33cfec311b26e79729f559fb641efea97a3ecee9
rnal09 <144640904+rnal09@users.noreply.github.com>
2023-10-19 07:39:52 +0700

    Create koneksi.php

---
+++ koneksi.php
@@ -0,0 +1,9 @@
+<?php 
+$koneksi =mysqli_connect("localhost","root","","db_website");
+
+// Chek connection
+if (mysqli_connect_errno()){
+    echo "koneksi database gagal : " . mysqli_connect_error();
+}
+
+?>
